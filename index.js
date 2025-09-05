c5fe44177f5664f2e269c54bed4bbdc359d3ae56
TaranChase <chase.taran@gmail.com>
2025-09-06 01:27:35 +0300

    Interacting with for loop and while loop within functions.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+// Assignmnet 1
+
+function writeCards(names, event) {
+    let messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return messages;
+}
+
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"]));
+
+// Assignment 2
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number --;
+    }
+}
+
